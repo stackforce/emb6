3f143fef22efa52696d2e18f91df2392b78a0439
Hamza Meddeb <hamza.meddeb@stackforce.de>
2018-05-18 16:44:08 +0200

    Add function to close the coap socket connection.

--- core/inc/apl/er-coap/er-coap.h
+++ core/inc/apl/er-coap/er-coap.h
@@ -178,6 +178,7 @@ extern coap_status_t erbium_status_code;
 extern char *coap_error_message;
 
 void coap_init_connection(uint16_t port, udp_socket_input_callback_t pf_coap_receive);
+void coap_close_connection(void);
 uint16_t coap_get_mid(void);
 
 void coap_init_message(void *packet, coap_message_type_t type, uint8_t code,
